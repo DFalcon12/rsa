8453a94277f8382e0212321b8484d0ebb59236d8
DFalcon12 <122582309+DFalcon12@users.noreply.github.com>
2024-02-19 00:39:34 -0600

    Add files via upload

--- firma.py
+++ firma.py
@@ -23,5 +23,42 @@ print("phiA", phiA, "\n")
 phiB = ((pB - 1) * (qB - 1))
 print("phiB", phiB, "\n")
 
+e = 65537
 
+dA = Crypto.Util.number.inverse(e, phiA)
+print("dA", dA , "\n")
+
+dB = Crypto.Util.number.inverse(e, phiB)
+print("dB", dB , "\n")
+
+msg = 'Hola mundo'
+print("Mensaje original: ", msg, "\n")
+print("Longitud del mensaje en bytes es: ", len(msg.encode('utf-8')))
+
+m = int.from_bytes(msg.encode('utf-8'), byteorder = 'big')
+print("mensaje convertido en entero: ", m ,"\n")
+
+c = pow(m,e,nB)
+print("Mensaje cifrado: ", c, "\n")
+
+des = pow(c, dB, nB)
+print ("Mensaje descifrado: ", des, "\n")
+
+msg_final = int.to_bytes(des, len(msg), byteorder = 'big').decode('utf-8')
+print("Mensaje final: ", msg_final, "\n")
+
+hashed_msg = hashlib.sha256(b'Hola mundo')
+print(f'\nhashed_msg: {hashed_msg}')
+
+mssg = int.from_bytes(hashed_msg.digest(), byteorder='big')
+sA = pow(mssg, dA, nA)
+print(f'Firma de alice: {sA}')
+
+firma = pow(sA, e, nA)
+print(f'Mensaje recibido de bob: {firma}')
+
+if firma == mssg:
+    print('Si hay firma')
+else:
+    print('Error')
 
